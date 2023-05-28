c450319f410aa13a70da37a1d51d49c7cb240e0d
Danila Isaichev <58274885+DanilaIsaichev@users.noreply.github.com>
2023-05-29 00:02:04 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] === "GET") {
+    echo '
 <html>
   <head>
     <title>PHP Test</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script>
     $(document).ready(function() {
-        $('#calcform').on('submit', function(e) {
+        $(`#calcform`).on(`submit`, function(e) {
             e.preventDefault();
             $.ajax({
-                url: '/index.php',
-                method: 'POST',
+                url: `/index.php`,
+                method: `POST`,
                 data: $(this).serialize(),
                 success: function(data) {
-                    $('#result').html(data);
+                    $(`#result`).html(data);
                 }
             });
         });
@@ -19,9 +22,7 @@
     </script>
   </head>
   <body>
-  <?php 
-    if ($_SERVER['REQUEST_METHOD'] === "GET") {
-      echo '<form id="calcform" name="calcform" action="index.php" method="POST"> 
+    <form id="calcform" name="calcform" action="index.php" method="POST"> 
     <input name="n1" type="number" placeholder="3" required pattern="1?[0-9]">
       <select name="op" id="operator">
         <option value="add">+</option>
@@ -33,36 +34,36 @@
     <input name="n2" type="number"  placeholder="4" required pattern="1?[0-9]">
     <input type="submit">
     <div id="result"></div>
-    </form>';
-    }
-
-    if ($_SERVER['REQUEST_METHOD'] === "POST") {
-      $a = $_POST['n1'];
-      $o = $_POST['op'];
-      $b = $_POST['n2'];
-      switch ($o) {
-        case "add":
-        $res = $a + $b;
-        echo "<p>{$a} + {$b} = {$res}</p>";
-        break;
-      case "substract":
-        $res = $a - $b;
-        echo "<p>{$a} - {$b} = {$res}</p>";
-        break;
-      case "multiply":
-        $res = $a * $b;
-        echo "<p>{$a} * {$b} = {$res}</p>";
-        break;
-      case "divide":
-        $res = $a / $b;
-        echo "<p>{$a} / {$b} = {$res}</p>";
-        break;
-      case "exponentiate":
-        $res = $a ** $b;
-        echo "<p>{$a} ^ {$b} = {$res}</p>";
-        break;
-      }
-    }
-  ?>
+    </form>
   </body>
-</html>
+</html>';
+  }
+
+  if ($_SERVER['REQUEST_METHOD'] === "POST") {
+    $a = $_POST['n1'];
+    $o = $_POST['op'];
+    $b = $_POST['n2'];
+    switch ($o) {
+      case "add":
+      $res = $a + $b;
+      echo "<p>{$a} + {$b} = {$res}</p>";
+      break;
+    case "substract":
+      $res = $a - $b;
+      echo "<p>{$a} - {$b} = {$res}</p>";
+      break;
+    case "multiply":
+      $res = $a * $b;
+      echo "<p>{$a} * {$b} = {$res}</p>";
+      break;
+    case "divide":
+      $res = $a / $b;
+      echo "<p>{$a} / {$b} = {$res}</p>";
+      break;
+    case "exponentiate":
+      $res = $a ** $b;
+      echo "<p>{$a} ^ {$b} = {$res}</p>";
+      break;
+    }
+  }
+?>
